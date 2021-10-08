ed4f5e45fef310b5f41c0f6b7a6096c39889c7dd
fktrcfylh1234567 <fktrcfylh1234567@yandex.ru>
2021-10-08 15:07:43 +0300

    add BluetoothServiceStunt

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
-from BluetoothService import BluetoothService
+from BluetoothServiceStunt import BluetoothServiceStunt
 
 
 def main():
-    service = BluetoothService()
+    service = BluetoothServiceStunt()
 
     for client_sock in service.listen_for_connections():
         msg = service.receive_from_client()
         print(msg)
         service.send_to_client("hello from Raspberry Pi4\n")
+        print("goodbye")
 
 
 if __name__ == "__main__":
